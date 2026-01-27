f712b78e8df9e516ab1f7680df3f51f24cc5a4bf
SHay <shaypi1986@gmail.com>
2026-01-27 15:08:13 +0200

    fix: terraform kubernetes provider authorization

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -68,10 +68,14 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command     = "aws"
+  }
 }
 
-resource "kubernetes_storage_class" "gp3" {
+resource "kubernetes_storage_class_v1" "gp3" {
   metadata {
     name = "gp3"
     annotations = {
