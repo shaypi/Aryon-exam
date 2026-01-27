ed54f338672823d1eaef0181a9c846988df0284d
SHay <shaypi1986@gmail.com>
2026-01-27 16:14:57 +0200

    fix: switch to exec auth with aws cli to prevent token expiration

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -68,7 +68,11 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+    command     = "aws"
+  }
 }
 
 resource "kubernetes_storage_class_v1" "gp3" {
