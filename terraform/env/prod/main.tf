00ee66fbdbce8ffeb5771d1f4daab2e15ffe6c7f
SHay <shaypi1986@gmail.com>
2026-01-28 03:06:00 +0200

    fix race condiiton

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -61,17 +61,9 @@ module "eks" {
   subnet_id               = module.vpc.private_subnets
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.aws_eks_cluster.cluster.name
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
