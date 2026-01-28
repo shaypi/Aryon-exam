7f4a673738ac8ea565e1c2ea891489b1506e3c4d
SHay <shaypi1986@gmail.com>
2026-01-28 02:14:59 +0200

    trying to fix  rollback

--- terraform/modules/eks/cluster.tf
+++ terraform/modules/eks/cluster.tf
@@ -26,7 +26,7 @@ resource "aws_eks_cluster" "main" {
 
   access_config {
     authentication_mode                         = "API_AND_CONFIG_MAP"
-    bootstrap_cluster_creator_admin_permissions = false
+    bootstrap_cluster_creator_admin_permissions = true
   }
 
   tags = merge(
