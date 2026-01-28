4812dc10e11492f8c8acab39fab311812db25bf0
SHay <shaypi1986@gmail.com>
2026-01-28 02:28:43 +0200

    trying to fix  rollback

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -73,8 +73,8 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
   exec {
-    api_version = "client.authentication.k8s.io/v1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name, "--region", var.region]
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
     command     = "aws"
   }
 }
